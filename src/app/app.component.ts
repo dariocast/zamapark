830fd14ae8b6692e3855f180bab72ba5148ca22a
Dario Castellano <dariocastellano18@gmail.com>
2019-10-11 00:13:46 +0200

    ghpages3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {PwaService} from './pwa-service.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'zamapark';
+
+  constructor(public Pwa: PwaService) {}
+  installPwa(): void {
+    this.Pwa.promptEvent.prompt();
+  }
 }
